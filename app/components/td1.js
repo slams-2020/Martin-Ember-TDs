786a320d3715c07ba28c3c1fd4a1dd69600820c2
Martin Jeanne <mjeanne@sio.local>
2021-03-15 15:33:50 +0100

    Second commit, but td1 is finished ! (yes i use git properly)

--- app/components/td1.js
+++ app/components/td1.js
@@ -3,32 +3,48 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class td1Component extends Component {
-    @tracked count = 2;
-    @tracked multiple = 1;
-
     @tracked text;
-    @tracked textLen = 100;
-
-    get total() {
-        return this.count + this.multiple;
-    }
-    @action
-    double() {
-        this.multiple = this.multiple * 2;
-    }
-    @action
-    change(amount) {
-        this.count = this.count + amount;
-    }
+    @tracked charAvailable = 100;
+    @tracked alert = "primary";
+    @tracked savedText = '';
+    @tracked saveDisplay = "none";
 
     @action
     countText() {
-        this.textLen = 100 - this.text.length;
+        this.charAvailable = 100 - this.text.length;
+        this.alertColor();
     }
 
     @action
     clear() {
         this.text = ''
-        this.textLen = 100;
+        this.charAvailable = 100;
+        this.alertColor();
+    }
+
+    @action
+    save(text) {
+        this.savedText = text;
+        if (this.savedText.length > 0) {
+            this.saveDisplay = "block";
+        }
+        else {
+            this.saveDisplay = "none";
+        }
+    }
+
+    alertColor() {
+        if (this.charAvailable < 10) {
+            this.alert = "danger";
+        }
+        else if (this.charAvailable < 30) {
+            this.alert = "warning";
+        }
+        else if (this.charAvailable < 50) {
+            return this.alert = "success";
+        }
+        else {
+            return this.alert = "primary";
+        }
     }
 }
